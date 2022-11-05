3ffbf1a85ad00660f08cd697ac8c5724ffbb9720
Adib Zefar <adibzefar@gmail.com>
2022-11-05 14:46:11 -0400

    main.rb

---
+++ main.rb
@@ -0,0 +1,8 @@
+require_relative 'tournament.rb'
+require_relative 'mathgame'
+require_relative 'player'
+require_relative 'question'
+require_relative 'turn'
+require 'colorize'
+
+Tournament.new.start
